04538ee472cba45e9f9ddc58f8c728b397450af5
koba928 <fd.18t.2705@s.thers.ac.jp>
2025-06-16 13:11:49 +0900

    Fix PostCSS nesting config for Vercel build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
+// postcss.config.js
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': require('@tailwindcss/nesting'), // ← ここも修正！
+    'tailwindcss/nesting': 'postcss-nesting', // ← ここ！プラグイン名を文字列で指定
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+
 
 
 
